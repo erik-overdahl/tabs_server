d3be24eb007145f9bf86023c15b4af62bcdafd9f
Erik Overdahl <erik.overdahl@gmail.com>
2023-02-28 15:29:27 -0600

    Add ValueEqual to deep compare by value

--- pkg/generate/util.go
+++ pkg/generate/util.go
@@ -167,3 +167,39 @@ func merge[T any](target, source T) T {
 	return target
 }
 
+func ValueEqual(x, y any) bool {
+	xVal := reflect.ValueOf(x)
+	yVal := reflect.ValueOf(y)
+	if !xVal.IsValid() || !yVal.IsValid() {
+		return !xVal.IsValid() && !yVal.IsValid()
+	}
+	typeOfX := xVal.Type()
+	if typeOfX != yVal.Type() {
+		return false
+	}
+	switch typeOfX.Kind() {
+	case reflect.Slice, reflect.Array:
+		if xVal.Len() != yVal.Len() {
+			return false
+		}
+		for i := 0; i < xVal.Len(); i++ {
+			if !ValueEqual(xVal.Index(i).Interface(), yVal.Index(i).Interface()) {
+				return false
+			}
+		}
+	case reflect.Pointer, reflect.Interface:
+		return ValueEqual(xVal.Elem().Interface(), yVal.Elem().Interface())
+	case reflect.Struct:
+		for i := 0; i < typeOfX.NumField(); i++ {
+			if !typeOfX.Field(i).IsExported() {
+				continue
+			}
+			if !ValueEqual(xVal.Field(i).Interface(), yVal.Field(i).Interface()) {
+				return false
+			}
+		}
+	default:
+		return xVal.Interface() == yVal.Interface()
+	}
+	return true
+}
---
+++ pkg/generate/util_test.go
@@ -0,0 +1,136 @@
+package generate
+
+import "testing"
+
+
+type greeter interface {
+	Greeting() string
+}
+
+type simpleStruct struct {
+	Name string
+}
+
+func (s simpleStruct) Greeting() string {
+	return s.Name
+}
+
+type ptrStruct struct {
+	Child *simpleStruct
+}
+
+type sliceStruct struct {
+	Children []simpleStruct
+}
+
+type ptrSliceStruct struct {
+	Children []*simpleStruct
+}
+
+type interfaceStruct struct {
+	Child greeter
+}
+
+type interfaceSliceStruct struct {
+	Children []greeter
+}
+
+func TestValueEqual(t *testing.T) {
+	cases := []struct{
+		Name string
+		X, Y any
+		Expected bool
+	}{
+		{"Equal strings", "foo", "foo", true},
+		{"Unequal strings", "foo", "bar", false},
+		{
+			"Equal structs",
+			simpleStruct{Name: "foo"},
+			simpleStruct{Name: "foo"},
+			true,
+		},
+		{
+			"Unequal structs",
+			simpleStruct{Name: "foo"},
+			simpleStruct{Name: "bar"},
+			false,
+		},
+		{
+			"Equal structs different pointers",
+			ptrStruct{Child: &simpleStruct{Name: "foo"}},
+			ptrStruct{Child: &simpleStruct{Name: "foo"}},
+			true,
+		},
+		{
+			"Unequal structs different pointers",
+			ptrStruct{Child: &simpleStruct{Name: "foo"}},
+			ptrStruct{Child: &simpleStruct{Name: "bar"}},
+			false,
+		},
+		{
+			"Equal string slice",
+			[]string{"foo"},
+			[]string{"foo"},
+			true,
+		},
+		{
+			"Unequal string slice",
+			[]string{"foo"},
+			[]string{"bar"},
+			false,
+		},
+		{
+			"Equal slice of structs",
+			[]simpleStruct{{"foo"}},
+			[]simpleStruct{{"foo"}},
+			true,
+		},
+		{
+			"Unequal slice of structs",
+			[]simpleStruct{{"foo"}},
+			[]simpleStruct{{"bar"}},
+			false,
+		},
+		{
+			"Equal slice as field on struct",
+			sliceStruct{Children: []simpleStruct{{"foo"}}},
+			sliceStruct{Children: []simpleStruct{{"foo"}}},
+			true,
+		},
+		{
+			"Unequal slice as field on struct",
+			sliceStruct{Children: []simpleStruct{{"foo"}}},
+			sliceStruct{Children: []simpleStruct{{"bar"}}},
+			false,
+		},
+		{
+			"Equal slice of pointers to structs",
+			[]*simpleStruct{{"foo"}},
+			[]*simpleStruct{{"foo"}},
+			true,
+		},
+		{
+			"Unequal slice of pointers to structs",
+			[]*simpleStruct{{"foo"}},
+			[]*simpleStruct{{"bar"}},
+			false,
+		},
+		{
+			"Equal slice of interfaces",
+			[]greeter{&simpleStruct{"foo"}},
+			[]greeter{&simpleStruct{"foo"}},
+			true,
+		},
+		{
+			"Unequal slice of interfaces",
+			[]greeter{&simpleStruct{"foo"}},
+			[]greeter{&simpleStruct{"bar"}},
+			false,
+		},
+	}
+	for _, c := range cases {
+		if actual := ValueEqual(c.X, c.Y); actual != c.Expected {
+			t.Errorf("%s: %#v, %#v: expected %v, got %v", c.Name, c.X, c.Y, c.Expected, actual)
+		}
+	}
+}
